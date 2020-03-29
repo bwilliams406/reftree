9c6b4801d776fec90e9e2378b1636a49eb4e1fc5
Brody Williams <bwilliams@electricitymanagementservices.com>
2020-03-29 17:05:49 -0500

    initial commit

---
+++ app.js
@@ -0,0 +1,726 @@
+jsonObject = {
+  "id": 3,
+  "name": "Peter Buell",
+  "children": [
+    {
+      "id": 4,
+      "name": "Chris Wielinski",
+      "children": [
+        {
+          "id": 21,
+          "name": "Steven Priddy"
+        },
+        {
+          "id": 22,
+          "name": "Jonathan Thayer"
+        },
+        {
+          "id": 23,
+          "name": "Mollye Bell"
+        },
+        {
+          "id": 24,
+          "name": "Terry Buell"
+        },
+        {
+          "id": 25,
+          "name": "Les Leonard"
+        },
+        {
+          "id": 26,
+          "name": "Christopher Surran"
+        },
+        {
+          "id": 27,
+          "name": "Jenny Garden"
+        },
+        {
+          "id": 28,
+          "name": "Darren Hayes"
+        },
+        {
+          "id": 30,
+          "name": "Mike Davis"
+        },
+        {
+          "id": 31,
+          "name": "Dee Greve"
+        }
+      ]
+    }
+    ,{
+      "id": 7,
+      "name": "Kyle Gribble",
+      "children": [
+        {
+          "id": 32,
+          "name": "Tom Wunderlick"
+        },
+        {
+          "id": 33,
+          "name": "John Summers"
+        },
+        {
+          "id": 34,
+          "name": "Steven Neuhoff"
+        },
+        {
+          "id": 35,
+          "name": "Rusty Miller"
+        },
+        {
+          "id": 36,
+          "name": "Rick Paulk"
+        },
+        {
+          "id": 37,
+          "name": "Ann O'connor"
+        },
+        {
+          "id": 38,
+          "name": "Marc Eller"
+        },
+        {
+          "id": 39,
+          "name": "Joseph Lynch"
+        },
+        {
+          "id": 40,
+          "name": "Kaylie Summers"
+        },
+        {
+          "id": 41,
+          "name": "Tony Dewar"
+        }
+      ]
+    }
+    ,{
+      "id": 8,
+      "name": "Scott Schneider",
+      "children": [
+        {
+          "id": 42,
+          "name": "Anthony Sherman"
+        },
+        {
+          "id": 43,
+          "name": "PBC Inc."
+        },
+        {
+          "id": 44,
+          "name": "Walt Miller"
+        },
+        {
+          "id": 45,
+          "name": "Leslie Caffey"
+        },
+        {
+          "id": 48,
+          "name": "Michael Edwin Crossley Jr."
+        },
+        {
+          "id": 49,
+          "name": "Kim Folse"
+        },
+        {
+          "id": 51,
+          "name": "Charles Gale"
+        },
+        {
+          "id": 55,
+          "name": "Michael Murray"
+        },
+        {
+          "id": 56,
+          "name": "Lee Belland"
+        },
+        {
+          "id": 57,
+          "name": "Alissa Stillman"
+        }
+      ]
+    }
+    ,{
+      "id": 12,
+      "name": "Jason Fondren",
+      "children": [
+        {
+          "id": 60,
+          "name": "Mary Marron"
+        },
+        {
+          "id": 61,
+          "name": "Ann Buell"
+        },
+        {
+          "id": 62,
+          "name": "Win Bell"
+        },
+        {
+          "id": 64,
+          "name": "Preston Berry"
+        },
+        {
+          "id": 65,
+          "name": "Chris Bracken"
+        },
+        {
+          "id": 67,
+          "name": "Kelly Parker"
+        },
+        {
+          "id": 68,
+          "name": "Art Allen"
+        },
+        {
+          "id": 69,
+          "name": "Michael Caddell"
+        },
+        {
+          "id": 70,
+          "name": "Suzanne Bowden"
+        },
+        {
+          "id": 71,
+          "name": "Anne Payne"
+        }
+      ]
+    }
+    ,{
+      "id": 13,
+      "name": "Beth Buell",
+      "children": [
+        {
+          "id": 73,
+          "name": "Karen Carney"
+        },
+        {
+          "id": 74,
+          "name": "Bob Buell"
+        },
+        {
+          "id": 75,
+          "name": "James Walkup"
+        },
+        {
+          "id": 76,
+          "name": "Anna Lyons"
+        },
+        {
+          "id": 78,
+          "name": "Chris Dobbs"
+        },
+        {
+          "id": 81,
+          "name": "Romaine Spinella"
+        },
+        {
+          "id": 82,
+          "name": "Terry Smith"
+        },
+        {
+          "id": 83,
+          "name": "Brian Fisher"
+        },
+        {
+          "id": 84,
+          "name": "Chipman Seale"
+        },
+        {
+          "id": 86,
+          "name": "Paul Buckley"
+        }
+      ]
+    }
+    ,{
+      "id": 14,
+      "name": "Todd Weber",
+      "children": [
+        {
+          "id": 87,
+          "name": "Blair Crossley"
+        },
+        {
+          "id": 88,
+          "name": "Frank Guidone"
+        },
+        {
+          "id": 90,
+          "name": "Jeff Smith"
+        },
+        {
+          "id": 92,
+          "name": "Halli Hockenjos"
+        },
+        {
+          "id": 93,
+          "name": "Suzanne Eubanks"
+        },
+        {
+          "id": 94,
+          "name": "Peter Flanders"
+        },
+        {
+          "id": 95,
+          "name": "Ryan Hughes"
+        },
+        {
+          "id": 96,
+          "name": "Sharla Myers"
+        },
+        {
+          "id": 97,
+          "name": "William Collins"
+        },
+        {
+          "id": 98,
+          "name": "Thomas Spaniel"
+        }
+      ]
+    }
+    ,{
+      "id": 16,
+      "name": "Edward Quinlan",
+      "children": [
+        {
+          "id": 99,
+          "name": "James Schmidt"
+        },
+        {
+          "id": 102,
+          "name": "Jeff Coker"
+        },
+        {
+          "id": 103,
+          "name": "Patrick Deen"
+        },
+        {
+          "id": 104,
+          "name": "Kristin Ciccarelli"
+        },
+        {
+          "id": 105,
+          "name": "Kurt Rodegast"
+        },
+        {
+          "id": 106,
+          "name": "Kerry Maguire"
+        },
+        {
+          "id": 107,
+          "name": "Tim Robinson"
+        },
+        {
+          "id": 108,
+          "name": "William Payne"
+        },
+        {
+          "id": 109,
+          "name": "Sam Viviano"
+        },
+        {
+          "id": 110,
+          "name": "Scott Wetzel"
+        }
+      ]
+    }
+    ,{
+      "id": 17,
+      "name": "Angie Lomeli",
+      "children": [
+        {
+          "id": 112,
+          "name": "Scott Smith"
+        },
+        {
+          "id": 113,
+          "name": "Danny Kearns"
+        },
+        {
+          "id": 114,
+          "name": "Pete Northway"
+        },
+        {
+          "id": 116,
+          "name": "Jerry Eden"
+        },
+        {
+          "id": 117,
+          "name": "Robert Black"
+        },
+        {
+          "id": 118,
+          "name": "Natalie Bedford"
+        },
+        {
+          "id": 119,
+          "name": "Tracy Castleman"
+        },
+        {
+          "id": 120,
+          "name": "Tracy Castleman"
+        },
+        {
+          "id": 121,
+          "name": "Rosa Lomeli"
+        },
+        {
+          "id": 122,
+          "name": "Michelle Wood"
+        }
+      ]
+    }
+    ,{
+      "id": 19,
+      "name": "Jerry Freeman",
+      "children": [
+        {
+          "id": 123,
+          "name": "Rory Sanders"
+        },
+        {
+          "id": 124,
+          "name": "Deborah Mcmurray"
+        },
+        {
+          "id": 126,
+          "name": "Raymond Maltese"
+        },
+        {
+          "id": 127,
+          "name": "Kerry Introligator"
+        },
+        {
+          "id": 130,
+          "name": "Jonathan Crane"
+        },
+        {
+          "id": 131,
+          "name": "Paula Reichenstein"
+        },
+        {
+          "id": 132,
+          "name": "Rick Obrien"
+        },
+        {
+          "id": 133,
+          "name": "Stephanie McCormick"
+        },
+        {
+          "id": 134,
+          "name": "John Young"
+        },
+        {
+          "id": 135,
+          "name": "Michelle Cook"
+        }
+      ]
+    }
+    ,{
+      "id": 20,
+      "name": "Michael Rilley",
+      "children": [
+        {
+          "id": 136,
+          "name": "Kevin Quinlan"
+        },
+        {
+          "id": 137,
+          "name": "Cynthia Brown"
+        },
+        {
+          "id": 138,
+          "name": "Tom Paredes"
+        }
+        ,
+        {
+          "id": 139,
+          "name": "Connie Criswell"
+        },
+        {
+          "id": 140,
+          "name": "Sydney Puddicombe"
+        },
+        {
+          "id": 141,
+          "name": "Richard Payne"
+        },
+        {
+          "id": 142,
+          "name": "David Ramsey"
+        },
+        {
+          "id": 143,
+          "name": "David Pearson"
+        },
+        {
+          "id": 144,
+          "name": "Wimberly Tribble"
+        },
+        {
+          "id": 145,
+          "name": "Nam Nguyen"
+        }
+      ]
+    }
+  ]
+}
+
+var canvas, stage, tree_container, branch_container, leaf_container,text_container;
+var firstNodeName, sky, ground, leaf;
+
+function init() {
+
+  manifest = [
+    {src: "./art/mdleafsgrp.png", id: "mdLeafGrp"},
+    {src: "./art/smleafred.png", id: "smLeafRed"},
+    {src: "./art/smleafgrn.png", id: "smLeafGrn"},
+    {src: "./art/smleaforg.png", id: "smLeafOrg"},
+    {src: "./art/smleafylw.png", id: "smLeafYlw"},
+    {src: "./art/treebark.png", id: "treebark"},
+    {src: "./art/boulder-drawing-bush-6.png", id: "treebush"},
+    {src: "./art/sky.png", id: "sky"},
+    {src: "./art/ground.png", id: "ground"},
+  ];
+
+  loader = new createjs.LoadQueue(false);
+  loader.addEventListener("complete", loadtree);
+  loader.loadManifest(manifest, true, "./art");
+
+          
+  loadtree();
+
+};
+
+function loadtree(){
+
+  const canvas = document.querySelector('canvas');
+
+  canvas.width = window.innerWidth * .6;
+  canvas.height = window.innerHeight*.9;
+
+  console.log('canvas width equals ' + canvas.width)
+
+
+  var stage     = new createjs.Stage("demoCanvas");
+  var tree_container = new createjs.Container();
+  var branch_container = new createjs.Container();
+  var leaf_container = new createjs.Container();
+  var text_container = new createjs.Container();
+  
+  firstNodeName = jsonObject.name;
+  firstNodeId   = jsonObject.id;
+
+
+//tree shrubery
+
+  // tree = new createjs.Bitmap(loader.getResult("treebush"));
+  // tree.x = 0;
+  // tree.y = 0;
+
+  // stage.addChild(tree);
+  // stage.update();
+ 
+
+              //if there is a name in the first spot, create a stump for the tree which represents account holder.
+              if(firstNodeName){
+                  var stump = new createjs.Shape();
+                  stump.graphics.beginBitmapFill(loader.getResult("treebark")).drawRect(canvas.width/2 - 20 , 700, canvas.width/2*.05, 300);
+                  // stump.graphics.beginFill("brown").drawRect(canvas.width/2 - 20 , 700, canvas.width/2*.05, 300);
+
+                  var first_node_name = new createjs.Text(jsonObject.name  , "12px Arial", "black");
+                    first_node_name.x = canvas.width/2 - 30;
+                    first_node_name.y = 800;
+                    first_node_name.textBaseline = "alphabetic";
+
+                  text_container.addChild(first_node_name);
+                  branch_container.addChild(stump);
+                  
+              };
+   
+              // D = length of json object
+              let D = jsonObject.children.length;
+
+              var Degrees = 90-(D-1) * 10;
+
+              // For each child object, create a new sqaure and draw it on the canvas
+              jsonObject.children.forEach(function(child, index) {
+                  x = canvas.width/2-12.5;
+                  y = 700;
+                  d = canvas.width/2*.3;
+                  var name = child.name;
+
+                  function degrees_to_radians(degrees){
+                    var pi = Math.PI;
+                    return degrees*(pi/180);
+                  }
+
+                  x2 = x+(d*Math.cos(degrees_to_radians(360 - Degrees)));
+                  y2 = y+(d*Math.sin(degrees_to_radians(360 - Degrees)));
+
+                  var stroke = 8;
+                  var color = "brown";
+
+                  var first_lvl_Branches = new createjs.Shape();
+
+                  first_lvl_Branches.graphics.clear().beginBitmapStroke(loader.getResult("treebark")).setStrokeStyle(stroke, 'butt', 'bevel').bezierCurveTo(x+9, y, x, y+25, x2+12, y2+11);
+
+                      // first_lvl_Branches.graphics.clear().setStrokeStyle(stroke, 'round', 'round').beginStroke(color).moveTo(x+9, y).bezierCurveTo(x+9, y, x, y+25, x2+12, y2+11);
+                      branch_container.addChild(first_lvl_Branches);
+
+                      first_lvl_leafs = new createjs.Bitmap(loader.getResult("mdLeafGrp"));
+                      first_lvl_leafs.x = x2+135;
+                      first_lvl_leafs.y = y2+20;
+                      first_lvl_leafs.rotation = 140;
+
+                      var first_lvl_name = new createjs.Text(name  , "8px Arial", "black");
+                      first_lvl_name.x = x2-13;
+                      first_lvl_name.y = y2+10;
+                      first_lvl_name.textBaseline = "alphabetic";
+
+                      first_lvl_leafs.addEventListener("click", handleClick);
+                        function handleClick(event){
+
+                        };
+
+                      text_container.addChild(first_lvl_name);
+                      leaf_container.addChild(first_lvl_leafs);
+
+                    Degrees += 19.8;
+
+//============================================================START OF SECOND LEVEL NODES===============================================================================
+
+                    var grandchild_index = 0;
+                        child.children.forEach(function(grandchild) {
+
+                          if(grandchild_index == 0 ){
+
+                            var d2 = canvas.width/2*.39;
+
+                            var Angle=22;
+
+                          }
+
+                          if(grandchild_index == 1 ){
+
+                            var d2 = canvas.width/2*.44;
+
+                            var Angle=13;
+
+                          }
+
+                          if(grandchild_index == 2 ){
+
+                            var d2 = canvas.width/2*.51;
+                            var Angle=22;
+                          }
+
+                          if(grandchild_index == 3 ){
+
+                            var d2 = canvas.width/2*.56;
+                            var Angle=15;
+                          }
+
+                          if(grandchild_index == 4 ){
+
+                            var d2 = canvas.width/2*.62;
+                            var Angle=22;
+                          }
+
+                          if(grandchild_index == 5 ){
+
+                            var d2 = canvas.width/2*.68;
+                            var Angle=15;
+                          }
+
+                          if(grandchild_index == 6 ){
+
+                            var d2 = canvas.width/2*.74;
+                            var Angle=21;
+                          }
+
+                          if(grandchild_index == 7 ){
+
+                            var d2 = canvas.width/2*.80;
+                            var Angle=14;
+                          }
+
+                          if(grandchild_index == 8 ){
+
+                            var d2 = canvas.width/2*.86;
+                            var Angle=20;
+                          }
+
+                          if(grandchild_index == 9 ){
+
+                            var d2 = canvas.width/2*.92;
+                            var Angle=13;
+                          }
+
+                          if(grandchild_index == 10 ){
+
+                            var d2 = canvas.width/2*.98;
+                             var Angle=19;
+                          }
+
+                            x3 = x+(d2*Math.cos(degrees_to_radians(Angle - Degrees)));
+                            y3 = y+(d2*Math.sin(degrees_to_radians(Angle - Degrees)));
+
+                              var stroke = 2;
+
+                              var second_lvl_Branches = new createjs.Shape();                            
+                              second_lvl_Branches.graphics.clear().beginBitmapStroke(loader.getResult("treebark")).setStrokeStyle(stroke, 'butt', 'bevel').moveTo(x2+11, y2+10).bezierCurveTo(x2+11, y2+10, x2, y2, x3+11, y3+10);
+                                branch_container.addChild(second_lvl_Branches);
+
+                                var redLeaf = loader.getResult("smLeafRed");
+                                var orgLeaf = loader.getResult("smLeafGrn");
+                                var ylwLeaf = loader.getResult("smLeafOrg");
+                                var grnLeaf = loader.getResult("smLeafYlw");
+                              
+                                const leafs = [redLeaf, orgLeaf, ylwLeaf, grnLeaf];
+                                const randomLeaf = leafs[Math.floor(Math.random() * leafs.length)];
+
+                              second_lvl_leafs = new createjs.Bitmap(randomLeaf);
+                                second_lvl_leafs.x = x3+35;
+                                second_lvl_leafs.y = y3+35;
+                                second_lvl_leafs.rotation = 140;
+
+                                var second_lvl_name = new createjs.Text(grandchild.name  , "8px Arial", "black");
+                                second_lvl_name.x = x3-13;
+                                second_lvl_name.y = y3+10;
+                                second_lvl_name.textBaseline = "alphabetic";
+
+                                second_lvl_leafs.addEventListener("click", handleClick);
+                                function handleClick(event){
+                                  console.log("2nd lvl" + grandchild.name)
+                                };
+                              text_container.addChild(second_lvl_name);
+                              leaf_container.addChild(second_lvl_leafs);
+
+
+                          grandchild_index ++;
+
+                        });
+
+                        child.children.forEach(function(grandchild) {
+
+
+                        })
+              });   
+
+
+
+
+              tree_container.addChild(branch_container, leaf_container); //text_container
+              stage.addChild(tree_container);
+              stage.update();
+
+
+              
+};  
+
+
+
+//     // child.children.forEach(function(grandchild) {
+//     //     // console.log(grandchild.name);
+//     // });
